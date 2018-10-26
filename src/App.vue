c526330f9aebad23f9e5cd47281c7381083b999e
Miya <qianwumeng_2cyndi@qq.com>
2018-10-27 04:28:11 +0800

    Update src/App.vue

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app">
+    <div class="fixed">
+      <logo />
+      <navigation />
+    </div>
+    <Header />
+    <wrap />
+    <Footer />
+  </div>
+</template>
+
+<script>
+import logo from './components/logo'
+import navigation from './components/navigation'
+import Header from './components/header'
+import wrap from './components/wrap'
+import Footer from './components/footer'
+export default {
+  name: 'App',
+  components: {
+    logo,
+    navigation,
+    Header,
+    wrap,
+    Footer
+  }
+}
+</script>
+
+<style>
+#app{
+  margin: 0;
+  padding: 0;
+  background-color: #e6e6e6;
+  font-size: 1rem;
+  letter-spacing: 1px;
+  overflow-x: hidden;
+}
+.fixed{
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    position: fixed;
+    width: 100%;
+    transition: .4s;
+}
+</style>
