718926c0fb188fce15d7df3acfb7c5d79ab8c570
Miya <qianwumeng_2cyndi@qq.com>
2018-10-27 04:28:30 +0800

    Update src/components/view-button.vue

---
+++ src/components/view-button.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <a :href="links">
+            <button>{{ message }}</button>
+        </a>
+    </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  props: {},
+  data () {
+    return {
+      links: 'https://github.com/Miya-Yukarin',
+      message: 'View Demo On GitHub'
+    }
+  },
+  watch: {},
+  computed: {},
+  methods: {},
+  created () {},
+  mounted () {}
+}
+</script>
+<style scoped>
+button{
+    padding: 16px;
+    border: transparent;
+    border-radius: 20px;
+    background-color: #00B4DB;
+    color: #fff;
+    transition: .6s;
+}
+button:hover{
+    transition: .6s;
+    box-shadow: 0 4px 8px rgba(0,180,219,.5);
+}
+button:active{
+    transition: .6s;
+    box-shadow: none;
+}
+</style>
