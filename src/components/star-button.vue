feacc52c03bf749eda55d997edbab5db07e50215
Miya <qianwumeng_2cyndi@qq.com>
2018-10-27 04:28:32 +0800

    Update src/components/star-button.vue

---
+++ src/components/star-button.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <a :href="links">
+            <button>
+                <i class="fa fa-star"> Star</i>
+            </button>
+        </a>
+    </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  props: {},
+  data () {
+    return {
+      links: 'https://github.com/Miya-Yukarin/Miya-Yukarin'
+    }
+  },
+  watch: {},
+  computed: {},
+  methods: {},
+  created () {},
+  mounted () {}
+}
+</script>
+<style scoped>
+</style>
