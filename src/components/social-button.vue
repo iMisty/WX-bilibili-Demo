dfaab11f9ca66ebef3c20b7768719f776ca508cd
Miya <qianwumeng_2cyndi@qq.com>
2018-10-27 04:28:34 +0800

    Update src/components/social-button.vue

---
+++ src/components/social-button.vue
@@ -0,0 +1,21 @@
+<template>
+<div></div>
+</template>
+
+<script>
+export default {
+  components: {},
+  props: {},
+  data () {
+    return {
+    }
+  },
+  watch: {},
+  computed: {},
+  methods: {},
+  created () {},
+  mounted () {}
+}
+</script>
+<style scoped>
+</style>
