f02cf3f0b4e41821d96f1c949c992968a5f24f7a
Miya <qianwumeng_2cyndi@qq.com>
2018-10-27 04:28:19 +0800

    Update src/components/footer.vue

---
+++ src/components/footer.vue
@@ -0,0 +1,37 @@
+<template>
+    <footer>
+        <logo />
+        <navigation />
+        <social-button />
+    </footer>
+</template>
+
+<script>
+import logo from './logo'
+import navigation from './navigation'
+import socialButton from './social-button'
+export default {
+  components: { logo, navigation, socialButton },
+  props: {},
+  data () {
+    return {
+    }
+  },
+  watch: {},
+  computed: {},
+  methods: {},
+  created () {},
+  mounted () {}
+}
+</script>
+<style scoped>
+footer{
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    padding: 2vh 0;
+}
+social-button a{
+    padding: 0 4px;
+}
+</style>
