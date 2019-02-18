657c56afaad91e20cabf219178371c9401d9d32a
Miya <qianwumeng_2cyndi@qq.com>
2019-02-18 15:54:14 +0800

    Delete introduce.js

--- components/introduce.js
+++
@@ -1,8 +0,0 @@
-import $ from 'jquery';
-import { introduce } from '../assets/js/option';
-
-function setIntroduce(){
-    $('#first-section').append(`<p>${introduce.text}</p>`);
-}
-
-export { setIntroduce }
